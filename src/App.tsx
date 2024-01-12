d5fd4b98bed34153e0b954ee8cd2ea43c8468706
cyb3r-anarch <aanon408@gmail.com>
2024-01-11 20:49:17 -0800

    inital project setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <body className='bg-slate-500'>
+      
+    <h1 className='text-lg'>Hello world</h1>
+    </body>
+    </>
+  )
+}
+
+export default App
