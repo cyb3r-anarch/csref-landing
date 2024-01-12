d50f78b744b453337250539435356ba58d580ffe
Elsdon Chang <elsdonc21@gmail.com>
2024-01-12 01:15:31 -0600

    dynamically render content for waitlist modal

--- src/components/WaitlistModal.tsx
+++ src/components/WaitlistModal.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import data from '../utils/data.json'
 
-function WaitlistModal( theme:string, onClose:() => void) {
+interface WaitlistModalProps {
+    curTheme: string;
+    onClose: () => void;
+}
+
+function WaitlistModal({ curTheme, onClose }: WaitlistModalProps ) {
+    
+    const themeObject = data.find(obj => obj[curTheme]);
+    const text = themeObject[curTheme].waitlistModalText;
+
+
     return (
         <div className="modal">
-          <h1>{`${data}.${theme}.waitlistModalText`}</h1>
+          <h1>{text}</h1>
           <input type="email" placeholder='email' />
-          <button>Sign Up</button>
+          <button type='submit'>Sign Up</button>
           <button onClick={onClose}>Close</button>
         </div>
     )
