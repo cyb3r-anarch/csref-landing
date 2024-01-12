fff69717aaaea1eb3c69e4451c221682b92dc95c
cyb3r-anarch <aanon408@gmail.com>
2024-01-11 22:18:22 -0800

    basic styling for cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import icon from '../assets/icon.png'
 function Card() {
   return (
-    <div>Card</div>
+  <>
+<div className="flex w-96 gap-x-5  px-6 py-3 border rounded-lg shadow-md">
+  <img style={{ maxHeight: "48px" }} src={icon} alt="" />
+  <div className="flex flex-col">
+    <h3 className="font-bold text-lg">Quick-onboarding</h3>
+    <p className="text-gray-600">
+      We have made it super easy for merchants to sign up and start accepting alternate payments
+    </p>
+  </div>
+</div>
+
+  </>
   )
 }
 
