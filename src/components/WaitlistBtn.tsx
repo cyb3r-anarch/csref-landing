640b3401881c62c0b5558876c7c0b959ece54fad
cyb3r-anarch <aanon408@gmail.com>
2024-01-11 22:41:16 -0800

    initial waitlist button styling

--- src/components/WaitlistBtn.tsx
+++ src/components/WaitlistBtn.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 function WaitlistBtn() {
   return (
-    <div>WaitlistBtn</div>
-  )
+    <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full">
+      Join Waitlist
+    </button>
+  );
 }
 
-export default WaitlistBtn
+export default WaitlistBtn;
