68d1637bd375b55cc923c46a4b87c8a807a29254
cyb3r-anarch <aanon408@gmail.com>
2024-01-16 19:36:33 -0800

    Optimize Card component for dynamic content using props

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import icon from '../assets/icon.png'
-function Card() {
+
+interface CardProps{
+  title: string;
+  description: string;
+}
+
+
+function Card({title, description}: CardProps) {
   return (
   <>
 <div className="flex w-96 gap-x-5  px-6 py-3 border rounded-lg shadow-md">
   <img style={{ maxHeight: "48px" }} src={icon} alt="" />
   <div className="flex flex-col">
-    <h3 className="font-bold text-lg">Quick-onboarding</h3>
+    <h3 className="font-bold text-lg">{title}</h3>
     <p className="text-gray-600">
-      We have made it super easy for merchants to sign up and start accepting alternate payments
+     {description} 
     </p>
   </div>
 </div>
