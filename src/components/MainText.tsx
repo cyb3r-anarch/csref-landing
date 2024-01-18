bc4063a036a8f21c2833d87a97a11d208b0ebf83
cyb3r-anarch <aanon408@gmail.com>
2024-01-17 16:49:29 -0800

    Adjusted styling

--- src/components/MainText.tsx
+++ src/components/MainText.tsx
@@ -9,7 +9,7 @@ export default function MainText({ curTheme }: MainTextProps) {
   return (
     <div className=' h-96 mt-32 ml-auto mb-16 mr-auto sm:container text-center px-96 gap-y-8 flex flex-col '>
       <h1 className='font-bold text-7xl'>Alternate Payments Made Easy</h1>
-      <p className='text-lg'>The most convinient way for merchants in Africa to easily accept mobile money, crypto, cash payments and third-party wallet services from their customers</p>
+      <p className='text-lg text-slate-300'>The most convinient way for merchants in Africa to easily accept mobile money, crypto, cash payments and third-party wallet services from their customers</p>
       <div className="min-w-8 ml-auto mr-auto font-light text-3xl">
         <WaitlistBtn text="Get Access Now" curTheme={curTheme}/>
       </div>
