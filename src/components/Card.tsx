f726dff7d1f2675810d2e221a12a8da601028276
Elsdon Chang <elsdonc21@gmail.com>
2024-01-17 20:21:25 -0600

    changed animations

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,30 +11,21 @@ interface CardProps{
 function Card({title, description}: CardProps) {
   return (
   <>
-    <motion.div
-      initial={{ opacity: 0, y: 20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.8 }}
-      className="flex w-96 gap-x-5  px-6 py-3 border rounded-lg shadow-md bg-blue-400/10"
+    <div
+      className={"flex w-96 gap-x-5  px-6 py-3 border rounded-lg shadow-md bg-blue-400/10 scale-up-bottom"}
     >
-      <motion.img
+      <img
         style={{ maxHeight: "48px" }}
         src={icon}
         alt=""
-        initial={{ opacity: 0, scale: 0.5 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 0.5 }}
       />
-      <motion.div
+      <div
         className="flex flex-col"
-        initial={{ opacity: 0, x: -20 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 0.8 }}
       >
         <h3 className="font-bold text-lg">{title}</h3>
         <p className="text-slate-300">{description}</p>
-      </motion.div>
-    </motion.div>
+      </div>
+    </div>
 
   </>
   )
