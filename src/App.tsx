048a3929e5f5b3e4be1c50f566dd423c85b6cf07
Elsdon Chang <elsdonc21@gmail.com>
2024-01-20 15:50:21 -0600

    changed how card renders

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import MainText from "./components/MainText";
 import Card from "./components/Card";
-import data from "./utils/data.json";
 import Confetti from "react-confetti";
 
 function App() {
@@ -14,6 +13,7 @@ function App() {
         localStorage.setItem("signedUp", "true");
         setIsSignedUp(true);
     };
+
     useEffect(() => {
         const signedUp = localStorage.getItem("signedUp") === "true";
         setIsSignedUp(signedUp);
@@ -23,68 +23,21 @@ function App() {
         setTheme(theme === "student" ? "engineer" : "student");
     };
 
-    // card content
-
-    // Safely access the first 'student' object's features, if it exists
-    const studentFeatures = data.find(obj => obj.student)?.student.features ?? [];
-
-    // Safely access the first 'engineer' object's features, if it exists
-    const engineerFeatures = data.find(obj => obj.engineer)?.engineer.features ?? [];
-
-    // Now, you can safely access the descriptions with checks
-    const studentTitle1 = studentFeatures[0]?.title;
-    const studentTitle2 = studentFeatures[1]?.title;
-    const studentTitle3 = studentFeatures[2]?.title;
-    const studentDescription1 = studentFeatures[0]?.description;
-    const studentDescription2 = studentFeatures[1]?.description;
-    const studentDescription3 = studentFeatures[2]?.description;
-
-    const engineerTitle1 = engineerFeatures[0]?.title;
-    const engineerTitle2 = engineerFeatures[1]?.title;
-    const engineerTitle3 = engineerFeatures[2]?.title;
-    const engineerDescription1 = engineerFeatures[0]?.description;
-    const engineerDescription2 = engineerFeatures[1]?.description;
-    const engineerDescription3 = engineerFeatures[2]?.description;
-
-
+    // array for Card keys
+    const cardKeys = [0, 1, 2];
 
     return (
         <div id="app">
-            {isSignedUp && <Confetti/>}
+            {isSignedUp && <Confetti />}
             <Navbar isSignedUp={isSignedUp} onSignUp={onSignUp} curTheme={theme} onThemeChange={themeChange} />
             <MainText isSignedUp={isSignedUp} onSignUp={onSignUp} curTheme={theme} />
-            {theme === "student" && (
-                <div className="w-full flex justify-around ">
-                    <Card
-                        title={studentTitle1}
-                        description={studentDescription1}
-                    />
-                    <Card
-                        title={studentTitle2}
-                        description={studentDescription2}
-                    />
-                    <Card
-                        title={studentTitle3}
-                        description={studentDescription3}
-                    />
-                </div>
-            )}
-            {theme === "engineer" && (
-                <div className="w-full flex justify-around ">
-                    <Card
-                        title={engineerTitle1}
-                        description={engineerDescription1}
-                    />
-                    <Card
-                        title={engineerTitle2}
-                        description={engineerDescription2}
-                    />
-                    <Card
-                        title={engineerTitle3}
-                        description={engineerDescription3}
-                    />
-                </div>
-            )}
+            <div className="w-full flex justify-around">
+                {// Map over cardKeys to render Card components
+                    cardKeys.map(key => (
+                        <Card curTheme={theme} key={key} itemKey={key}/>
+                    ))
+                }
+            </div>
         </div>
     );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,32 +1,32 @@
 import React from 'react'
 import icon from '../assets/icon.png'
-import { motion } from 'framer-motion';
+import data from '../utils/data.json'
 
 interface CardProps{
-  title: string;
-  description: string;
+  curTheme: string;
+  itemKey: number;
 }
 
+function Card(this: any, { curTheme, itemKey }: CardProps) {
+  // Card Content from JSON file
+  const studentFeatures = data.find(obj => obj.student)?.student.features ?? [];
+  const engineerFeatures = data.find(obj => obj.engineer)?.engineer.features ?? [];
 
-function Card({title, description}: CardProps) {
+  const studentTitle = studentFeatures[itemKey]?.title;
+  const studentDescription = studentFeatures[itemKey]?.description;
+
+  const engineerTitle = engineerFeatures[itemKey]?.title;
+  const engineerDescription = engineerFeatures[itemKey]?.description;
+
   return (
   <>
-    <div
-      className={"flex w-96 gap-x-5  px-6 py-3 border rounded-lg shadow-md bg-blue-400/10 scale-up-bottom"}
-    >
-      <img
-        style={{ maxHeight: "48px" }}
-        src={icon}
-        alt=""
-      />
-      <div
-        className="flex flex-col"
-      >
-        <h3 className="font-bold text-lg">{title}</h3>
-        <p className="text-slate-300">{description}</p>
+    <div className={"flex w-96 gap-x-5  px-6 py-3 border rounded-lg shadow-md bg-blue-400/10 scale-up-bottom"}>
+      <img style={{ maxHeight: "48px" }} src={icon} alt=""/>
+      <div className="flex flex-col">
+        <h3 className="font-bold text-lg">{curTheme==="student" ? studentTitle : engineerTitle}</h3>
+        <p className="text-slate-300">{curTheme==="student" ? studentDescription : engineerDescription}</p>
       </div>
     </div>
-
   </>
   )
 }
